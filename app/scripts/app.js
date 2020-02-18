ac4e9807fa91c3d28a4611926451fc3c691b6314
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-18 11:28:12 -0300

    Done the Ninjas page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,36 +2,54 @@ var app = angular.module('myApp', []);
 
 app.controller('Controller', function ($scope) {
 
-$scope.removeNinja = function(ninja){
-    var removedNinja = $scope.ninjas.indexOf(ninja)
-    //The '1' specifie to remove just one element on the array
-    $scope.ninjas.splice(removedNinja, 1);
-}
+    $scope.removeNinja = function (ninja) {
+        var removedNinja = $scope.ninjas.indexOf(ninja)
+        //The '1' specifie to remove just one element on the array
+        $scope.ninjas.splice(removedNinja, 1);
+    }
+
+    $scope.addNinja = function(){
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate),
+            available: true
+        })
+
+        $scope.newninja.name = "";
+        $scope.newninja.belt = "";
+        $scope.newninja.rate = "";
+
+    };
 
     $scope.ninjas = [
         {
             name: "Yoshi",
-            belt:   "Green",
+            belt: "Green",
             rate: 50,
-            available: true
+            available: true,
+            thumb: "content/img/icon.png"
         },
         {
             name: "Crystal",
             belt: "Yellow",
             rate: 30,
-            available: true
+            available: true,
+            thumb: "content/img/icon.png"
         },
         {
             name: "Ryu",
             belt: "Orange",
             rate: 20,
-            available: false
+            available: false,
+            thumb: "content/img/icon.png"
         },
         {
             name: "Shaun",
             belt: "Black",
             rate: 1000,
-            available: true
+            available: true,
+            thumb: "content/img/icon.png"
         }
     ];
 });
