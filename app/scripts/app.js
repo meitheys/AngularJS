a881eb003f4b68a6851a49f4c1bc6b1dc48b6636
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-18 09:33:03 -0300

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('myApp', []);
+
+app.controller('Controller', function ($scope) {
+    $scope.alunos = [
+        {
+            name: "Yoshi",
+            belt:   "Green"
+        },
+        {
+            name:
+            belt:
+        }
+
+    ];
+});
