e4cb85f6262c96709be978fafa51216d9189909a
Matheus Furtado <50843672+meitheys@users.noreply.github.com>
2020-02-18 11:00:07 -0300

    Adding functions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,37 @@
 var app = angular.module('myApp', []);
 
 app.controller('Controller', function ($scope) {
-    $scope.alunos = [
+
+$scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja)
+    //The '1' specifie to remove just one element on the array
+    $scope.ninjas.splice(removedNinja, 1);
+}
+
+    $scope.ninjas = [
         {
             name: "Yoshi",
-            belt:   "Green"
+            belt:   "Green",
+            rate: 50,
+            available: true
         },
         {
-            name:
-            belt:
+            name: "Crystal",
+            belt: "Yellow",
+            rate: 30,
+            available: true
+        },
+        {
+            name: "Ryu",
+            belt: "Orange",
+            rate: 20,
+            available: false
+        },
+        {
+            name: "Shaun",
+            belt: "Black",
+            rate: 1000,
+            available: true
         }
-
     ];
 });
